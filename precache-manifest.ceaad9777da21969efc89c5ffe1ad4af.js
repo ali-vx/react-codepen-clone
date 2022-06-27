4a4101078923a46d0033d2e05edb921f9f4e5cd6
Muhammad Ali <amuhammad161@gmail.com>
2022-06-27 19:49:14 +0500

    Updates

---
+++ precache-manifest.ceaad9777da21969efc89c5ffe1ad4af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8890da3b3cbeb26156b819f20e9f372",
+    "url": "/react-codepen-clone/index.html"
+  },
+  {
+    "revision": "7e6ae001feb225c107e8",
+    "url": "/react-codepen-clone/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "8d5e258cd5f0822e4b05",
+    "url": "/react-codepen-clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "7e6ae001feb225c107e8",
+    "url": "/react-codepen-clone/static/js/2.31763677.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/react-codepen-clone/static/js/2.31763677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5e258cd5f0822e4b05",
+    "url": "/react-codepen-clone/static/js/main.07a26d8a.chunk.js"
+  },
+  {
+    "revision": "583799b05e88ee4ecca8",
+    "url": "/react-codepen-clone/static/js/runtime-main.1fe374de.js"
+  }
+]);
